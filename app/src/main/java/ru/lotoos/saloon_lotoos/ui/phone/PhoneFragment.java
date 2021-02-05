cd26941ed68d98a60dfd064493fecb994f6e7725
RudiPachanin <rudi.pach@ya.ru>
2021-02-05 20:57:21 +0300

    Call phone button

--- app/src/main/java/ru/lotoos/saloon_lotoos/ui/phone/PhoneFragment.java
+++ app/src/main/java/ru/lotoos/saloon_lotoos/ui/phone/PhoneFragment.java
@@ -1,9 +1,13 @@
 package ru.lotoos.saloon_lotoos.ui.phone;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -19,6 +23,19 @@ public class PhoneFragment extends Fragment {
         phoneViewModel =
                 new ViewModelProvider(this).get(PhoneViewModel.class);
         View root = inflater.inflate(R.layout.fragment_phone, container, false);
+
+        Button button=(Button) root.findViewById(R.id.callBaton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel: +7(962)992-64-64"));
+                PhoneFragment.this.getActivity().startActivity(intent);
+            }
+        });
+
         return root;
+
     }
+
 }
